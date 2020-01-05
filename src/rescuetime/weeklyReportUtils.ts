c8ff3be484a02aa54875e3d3dd271d3133869a3e
stardustrain <kthanterran@gmail.com>
2020-01-05 23:14:12 +0900

    Add weekly data parse functions

--- src/rescuetime/weeklyReportUtils.ts
+++ src/rescuetime/weeklyReportUtils.ts
@@ -1,5 +1,26 @@
-import { Overview } from "../@types/models"
+import { take, map } from 'ramda'
 
-export const parseOverview = (overview: Overview) => {
-  
-}
+import { Overview, Activity, Efficiency } from '../@types/models'
+
+export const parseOverview = (overview: Overview) =>
+  map((row) => ({
+    rank: row[0],
+    timeSpent: row[1],
+    category: row[3],
+  }), take(5, overview.rows))
+
+export const parseActivity = (activity: Activity) =>
+  map((row) => ({
+    rank: row[0],
+    timeSpent: row[1],
+    activity: row[3],
+    category: row[4],
+    weightedProductivty: row[5],
+  }), take(5, activity.rows))
+
+export const parseEffieciency = (efficiency: Efficiency) =>
+  map((row) => ({
+    rank: row[0],
+    timeSpent: row[1],
+    efficiency: row[3],
+  }), efficiency.rows)
