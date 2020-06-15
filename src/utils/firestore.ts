d1709de44496b5adc0bc05a48ffbc746331ad41e
stardustrain <kthanterran@gmail.com>
2020-06-16 00:12:18 +0900

    resolve #18 Add logic for writing daily report data to Cloud firestore

---
+++ src/utils/firestore.ts
@@ -0,0 +1,40 @@
+import admin from 'firebase-admin'
+import { isNil } from 'ramda'
+import dayjs from 'dayjs'
+
+import { FirestoreInsertData } from '../@types/models'
+
+const initializeFirestore = () => {
+  const serviceAccount = process.env.GCP_SA_KEY
+
+  if (isNil(serviceAccount)) {
+    throw Error('Does not received service account')
+  }
+
+  const parsedServiceAccount = JSON.parse(Buffer.from(serviceAccount, 'base64').toString())
+
+  admin.initializeApp({
+    credential: admin.credential.cert(parsedServiceAccount),
+  })
+
+  return admin.firestore()
+}
+
+export const setDailyData = async (data: FirestoreInsertData) => {
+  try {
+    const db = initializeFirestore()
+    const formattedDate = dayjs(data.date).utc().format()
+
+    const docRef = db.collection('rescuetime').doc(formattedDate)
+    const insertData = {
+      ...data,
+      date: formattedDate,
+    }
+
+    await docRef.set(insertData)
+
+    console.info(`Write firestore data: ${JSON.stringify(insertData)}`)
+  } catch (e) {
+    console.error(e)
+  }
+}
--- src/webhook/index.ts
+++ src/webhook/index.ts
@@ -1,59 +1,90 @@
 import { IncomingWebhook } from '@slack/webhook'
+import { values, all, equals } from 'ramda'
 
 import { getDailyData, getWeeklyData } from '../rescuetime'
-import { generateTodayMessage, generateWeeklyMessage } from './messages'
+import { generateTodayMessage, generateWeeklyMessage, getDataEmptyMessage } from './messages'
 import { dailyMessageBlock, weeklyMessageBlock } from './messageBlocks'
 import { getToday, getWeekRange } from '../utils/misc'
 
 const HOOK_URL = process.env.HOOK_URL || ''
 
 const webhook = new IncomingWebhook(HOOK_URL)
+const allEqualsZero = all(equals(0))
 
 export const sendDailyWebHook = async () => {
-  const dailyData = await getDailyData()
-  const { totalHour, productiveTime, distractingTime, devTime } = generateTodayMessage(dailyData?.summary, dailyData?.compareYesterday)
-  const currentDate = dailyData?.date ?? getToday().subtract(1, 'day').format('YYYY-MM-DD')
-
   try {
-    await webhook.send(dailyMessageBlock({
-      totalHour,
-      productiveTime,
-      distractingTime,
-      devTime,
-      currentDate,
-    }))
-    console.info(`Send daily webhook data: ${JSON.stringify({
-      currentDate,
-      totalHour,
-      productiveTime,
-      distractingTime,
-      devTime,
-    })}`)
+    const dailyData = await getDailyData()
+    const currentDate = dailyData.date ?? getToday().subtract(1, 'day').format('YYYY-MM-DD')
+
+    if (allEqualsZero(values(dailyData.summary))) {
+      await webhook.send({
+        text: 'It does not exist rescuetime data.',
+        blocks: getDataEmptyMessage(),
+      })
+
+      return {
+        date: currentDate,
+        data: null,
+      }
+    }
+
+    const { totalHour, productiveTime, distractingTime, devTime } = generateTodayMessage(
+      dailyData.summary,
+      dailyData.compareYesterday,
+    )
+
+    await webhook.send(
+      dailyMessageBlock({
+        totalHour,
+        productiveTime,
+        distractingTime,
+        devTime,
+        currentDate,
+      }),
+    )
+    console.info(
+      `Send daily webhook data: ${JSON.stringify({
+        currentDate,
+        totalHour,
+        productiveTime,
+        distractingTime,
+        devTime,
+      })}`,
+    )
+
+    return {
+      date: currentDate,
+      data: dailyData.summary,
+    }
   } catch (e) {
     console.error(e)
   }
 }
 
 export const sendWeeklyWebHook = async () => {
-  const weeklyData = await getWeeklyData()
-  const { totalHour, efficiencyRank, overviewRank } = generateWeeklyMessage(weeklyData)
-  const { from, to } = getWeekRange()
-
   try {
-    await webhook.send(weeklyMessageBlock({
-      from: weeklyData?.from ?? from,
-      to: weeklyData?.to ?? to,
-      totalHour,
-      efficiencyRank,
-      overviewRank,
-    }))
-    console.info(`Send weekly webhook data: ${JSON.stringify({
-      from: weeklyData?.from,
-      to: weeklyData?.to,
-      totalHour,
-      efficiencyRank,
-      overviewRank,
-    })}`)
+    const weeklyData = await getWeeklyData()
+    const { totalHour, efficiencyRank, overviewRank } = generateWeeklyMessage(weeklyData)
+    const { from, to } = getWeekRange()
+
+    await webhook.send(
+      weeklyMessageBlock({
+        from: weeklyData?.from ?? from,
+        to: weeklyData?.to ?? to,
+        totalHour,
+        efficiencyRank,
+        overviewRank,
+      }),
+    )
+    console.info(
+      `Send weekly webhook data: ${JSON.stringify({
+        from: weeklyData?.from,
+        to: weeklyData?.to,
+        totalHour,
+        efficiencyRank,
+        overviewRank,
+      })}`,
+    )
   } catch (e) {
     console.error(e)
   }
